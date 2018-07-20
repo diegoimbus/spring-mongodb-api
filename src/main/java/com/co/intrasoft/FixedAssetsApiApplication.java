438d83fa11176c026d9e67aa65124601cd29bd22
Diego Imbus <dsn.imbus01@gmail.com>
2018-07-20 15:51:03 -0500

    spring project created with maven dependencies

---
+++ src/main/java/com/co/intrasoft/FixedAssetsApiApplication.java
@@ -0,0 +1,12 @@
+package com.co.intrasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedAssetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedAssetsApiApplication.class, args);
+	}
+}
