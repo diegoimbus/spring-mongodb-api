fd99f33276c125d05ed327867cdd95636403a5b7
Diego Imbus <dsn.imbus01@gmail.com>
2018-07-20 16:33:32 -0500

    preliminary document without date

---
+++ src/main/java/com/co/intrasoft/documents/Client.java
@@ -0,0 +1,5 @@
+package com.co.intrasoft.documents;
+
+public class Client {
+
+}
---
+++ src/main/java/com/co/intrasoft/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.co.intrasoft.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ClientRepository extends MongoRepository<T, ID> {
+
+}
